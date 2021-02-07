264a85cb1391323c9eb0745680f0b21e6d338a76
Manikant Rai <manikant.rai2000@gmail.com>
2021-02-07 11:30:49 +0530

    start button working

--- app/src/main/java/com/example/healthylifestyle/MainActivity.kt
+++ app/src/main/java/com/example/healthylifestyle/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.healthylifestyle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.healthylifestyle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.llstart.setOnClickListener {
+            Toast.makeText(this, "Here we will start the excersise",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
